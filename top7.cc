fc7a9b0eeb0f7ae2d8f3445e4c62c70a09477db7
Jan Roguwski <Jroguwski@gmail.com>
2022-10-20 22:43:42 +0200

    Poprawienie interpunkcji komentarzy

--- top7.cc
+++ top7.cc
@@ -21,7 +21,7 @@ using std::cin;
 using std::cout;
 using std::cerr;
 
-/* Type definitions */
+/* Type definitions. */
 using line_t = string;
 using line_number_t = uint64_t;
 
@@ -38,11 +38,11 @@ using top_t = unordered_map<song_num_t, points_t>;
 using song_list_t = vector<song_num_t>;
 
 
-/* Global variables */
+/* Global variables. */
 namespace {
-    constexpr size_t ranking_length = 7; // length of ranking
+    constexpr size_t ranking_length = 7; // Length of ranking.
     constexpr song_num_t max_song_number = 99999999;
-    song_num_t current_max_song = 0; // max currently allowed song number
+    song_num_t current_max_song = 0; // Maximal currently allowed song number.
 
     charts_t current_chart;
     top_t top_songs;
@@ -53,9 +53,9 @@ namespace {
 
     song_list_t last_chart, last_top_call;
 
-    bool open_voting = false; // was there any NEW already
+    bool open_voting = false; // Was there any NEW already?
 
-    // Comparison struct for element ordering purposes
+    // Comparison struct for element ordering purposes.
     struct cmp {
         bool operator()(const chart_elem_t &a, const chart_elem_t &b) const {
             return a.second > b.second ||
@@ -65,7 +65,7 @@ namespace {
 }
 
 
-// Writes top7 songs either all-time or in new chart
+// Writes top7 songs either all-time or in new chart.
 void writeTop7(const set<chart_elem_t, cmp> &bestSongs, const song_list_t &last) {
     int currentRank = 1;
 
@@ -83,8 +83,8 @@ void writeTop7(const set<chart_elem_t, cmp> &bestSongs, const song_list_t &last)
 
 
 
-/* Algorithms */
-// Write votes in chart and checks for correctness
+/* Algorithms. */
+// Write votes in chart and checks for correctness.
 void vote(const song_set_t &votes) {
     for_each(votes.begin(), votes.end(), [](auto song) { ++current_chart[song]; });
 }
@@ -92,7 +92,7 @@ void vote(const song_set_t &votes) {
 // Write out top 7 songs from chart, reject some songs, and reset chart.
 void summarize() {
     if (current_max_song > 0) {
-        // Get top7 songs from current_chart using set
+        // Get top7 songs from current_chart using set.
         set<chart_elem_t, cmp> bestSongs;
 
         for (auto const &[song, votes]: current_chart) {
@@ -102,17 +102,17 @@ void summarize() {
             }
         }
 
-        // Write out top 7 songs from chart
+        // Write out top 7 songs from chart.
         writeTop7(bestSongs, last_chart);
 
-        // Update top_songs
+        // Update top_songs.
         points_t points = ranking_length;
         for (const auto &[song, votes]: bestSongs) {
             top_songs[song] += points;
             points--;
         }
 
-        // Reject some songs
+        // Reject some songs.
         song_set_t bestSongsUS;
         for (const auto &[song, votes]: bestSongs) {
             bestSongsUS.emplace(song);
@@ -123,10 +123,10 @@ void summarize() {
             }
         }
 
-        // Reset the chart
+        // Reset the chart.
         current_chart.clear();
 
-        // Update last_chart
+        // Update last_chart.
         last_chart.clear();
         for (const auto &[song, votes]: bestSongs) {
             last_chart.emplace_back(song);
@@ -134,9 +134,9 @@ void summarize() {
     }
 }
 
-// Gets all-time top7 charts hits
+// Gets all-time top7 charts hits.
 void top() {
-    // Get top7 songs from top_songs using set
+    // Get top7 songs from top_songs using set.
     set<top_elem_t, cmp> best_songs;
     for (const auto &[song, points]: top_songs) {
         best_songs.emplace(song, points);
@@ -145,22 +145,22 @@ void top() {
         }
     }
 
-    // Write top songs
+    // Write top songs.
     writeTop7(best_songs, last_top_call);
 
-    // Update last_top_call array
+    // Update last_top_call array.
     last_top_call.clear();
     for (const auto &[song, pts]: best_songs) {
         last_top_call.emplace_back(song);
     }
 
     // Reject from top_songs rejected songs which have not made it
-    // to the top7 ranking since they will never be in top7 again
+    // to the top7 ranking since they will never be in top7 again.
     for (auto it = top_songs.begin(); it != top_songs.end();) {
-        if (rejected_songs.contains(it->first)) { // already rejected
+        if (rejected_songs.contains(it->first)) { // Already rejected.
             auto iter = find(last_top_call.begin(), last_top_call.end(),
                              it->first);
-            if (iter == last_top_call.end()) { // not in current top standings
+            if (iter == last_top_call.end()) { // Not in current top standings.
                 it = top_songs.erase(it);
             } else {
                 it++;
@@ -172,15 +172,15 @@ void top() {
 }
 
 
-/* Input parsing */
-// Writes out the line in which error occurred
+/* Input parsing. */
+// Writes out the line in which error occurred.
 void wrong_line() {
     cerr << "Error in line " << line_number << ": " << line << "\n";
 }
 
-// Reads votes and casts them if no error is found
+// Reads votes and casts them if no error is found.
 void cast_votes() {
-    if (!open_voting) { // if NEW wasn't called yet
+    if (!open_voting) { // If NEW wasn't called yet.
         wrong_line();
     } else {
         stringstream ss(line);
@@ -189,14 +189,15 @@ void cast_votes() {
         while (ss >> song) {
             if (song == 0 || song > current_max_song ||
                     rejected_songs.contains(song) || votes.contains(song)) {
-                // if error in conversion, song is rejected or already voted for
+                // If error in conversion occurs, song is rejected or
+                // already voted for.
                 wrong_line();
                 return;
             }
             votes.emplace(song);
         }
         if (song == 0 || song == numeric_limits<song_num_t>::max()) {
-            // it means there was an error in a line
+            // It means there was an error in a line.
             wrong_line();
             return;
         }
@@ -204,17 +205,18 @@ void cast_votes() {
     }
 }
 
-// Checks next chart max song number and outputs new chart according
+// Checks next chart max song number and outputs new chart according.
 void new_charts(stringstream &ss) {
     string temp;
     song_num_t maximum;
-    ss >> maximum; // reads max song number
+    ss >> maximum; // Reads max song number.
     if (ss >> temp || maximum == 0 || maximum < current_max_song ||
             maximum > max_song_number) {
-        // something more in line, error in conversion or song number is incorrect
+        // Something more in line, error in conversion or song number
+        // is incorrect.
         wrong_line();
     } else {
-        if (open_voting) { // if NEW was invoked before create a new chart
+        if (open_voting) { // If NEW was invoked before create a new chart.
             summarize();
         }
         current_max_song = maximum;
@@ -222,28 +224,28 @@ void new_charts(stringstream &ss) {
     }
 }
 
-// Checks if nothing more is in line and invokes top chart
+// Checks if nothing more is in line and invokes top chart.
 void write_top_songs(stringstream &ss) {
     string temp;
-    if (ss >> temp) { // something more than TOP in line
+    if (ss >> temp) { // Something more than "TOP" in line.
         wrong_line();
     } else {
         top();
     }
 }
 
-// Parses input (not via regex) and invokes suitable function
+// Parses input (not via regex) and invokes suitable function.
 void parse() {
     ++line_number;
     stringstream ss(line);
     string word;
     ss >> word;
-    if (word == "NEW") { // if NEW
+    if (word == "NEW") { // If NEW.
         new_charts(ss);
-    } else if (word == "TOP") { // if TOP
+    } else if (word == "TOP") { // If TOP.
         write_top_songs(ss);
     } else if (!all_of(line.begin(), line.end(), isspace)) {
-        // if not NEW or TOP but not only whitespaces
+        // If not NEW or TOP but not only whitespaces.
         cast_votes();
     }
 }
